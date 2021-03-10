d989a7a34ade55b6da6e00190129d3a4476cf7ac
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-03-09 19:26:06 -0600

    enable flash messages display

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,4 +1,8 @@
 console.log("loaded");
 $(document).on("turbolinks:load", () => {
   $(".ui.dropdown").dropdown();
+
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
 });
