60f75a5fa7e902d6713dfd81f53bd4909d773e0d
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-03-09 19:48:54 -0600

    create messages from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "chatroom#index"
+  post "message", to: "messages#create"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
