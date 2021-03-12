c4f4b10fcdf35ad086c51169450345bbfb788ded
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-03-11 21:31:37 -0600

    feat(controller) : new after_action for sessions#create - when an user logins, notify other users with welcome message in the chat

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   before_action :logged_in_redirect, only: [:new, :create]
+  after_action :welcome_message, only: [:create]
 
   def new
   end
@@ -9,7 +10,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to root_path
+      redirect_to(root_path)
+      return
     else
       flash.now[:error] = "There was soemthinmg wrong with your login information"
       render "new"
@@ -29,5 +31,14 @@ class SessionsController < ApplicationController
       flash[:error] = "You have already logged in"
       redirect_to root_path
     end
+  end
+
+  def welcome_message
+    ActionCable.server.broadcast "chatroom_channel", mod_message: message_render("Everyone welcome #{current_user.username}")
+  end
+
+  def message_render(message)
+    # render(partial: "message", locals: { message: message })
+    MessagesController.render(partial: "message", locals: { message: message })
   end
 end
