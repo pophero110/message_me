11beeee4f6b007865fe28cb0719cde1445d3457a
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-03-13 22:11:29 -0600

    feat(controller) create an user with signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "Your account has been successfully created!"
+      redirect_to login_path
+    else
+      flash[:error] = "Oops something went wrong"
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
