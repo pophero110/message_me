a0e2455c5d6fdd31af365de1b5bd888b17e57686
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-03-29 15:46:26 -0500

    feat(connection)  authenicate user before register connenction

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,12 +1,17 @@
 import consumer from "./consumer";
 
 consumer.subscriptions.create("ChatroomChannel", {
+  initialized() {
+    // Called before page loaded
+  },
   connected() {
     // Called when the subscription is ready for use on the server
+    alert("connected");
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    alert("disconnected");
   },
 
   received(data) {
