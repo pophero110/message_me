d9ff851bfe3e37d9d1a5696a84d2983604e5fc32
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2021-03-29 15:55:23 -0500

    remove useless code

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -6,12 +6,10 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
   connected() {
     // Called when the subscription is ready for use on the server
-    alert("connected");
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
-    alert("disconnected");
   },
 
   received(data) {
